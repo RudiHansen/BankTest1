3fd0f90d0f3b979477d9016052a3b913648ddcbe
RudiHansen <rsh@pobox.com>
2014-02-10 13:50:55 +0100

    Første eksempel fra bogen

---
+++ BankTest1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTest1
+{
+    class Program
+    {
+        public interface IAccount
+        {
+            void PayInFunds(decimal amount);
+            bool WithdrawFunds(decimal amount);
+            decimal GetBalance();
+            void SetBalance(decimal amount);
+            string RudeLetterString();
+            string GetName();
+            void SetName(string accountName);
+        }
+        public abstract class Account : IAccount
+        {
+            private string name; 
+            private decimal balance = 0;
+
+            public abstract string RudeLetterString();
+
+            public virtual bool WithdrawFunds(decimal amount)
+            {
+                if (balance < amount)
+                {
+                    return false;
+                }
+                balance = balance - amount; return true;
+            }
+            public decimal GetBalance()
+            {
+                return balance;
+            }
+            public void SetBalance(decimal amount)
+            {
+                balance = amount;
+            }
+            
+            public void PayInFunds(decimal amount)
+            {
+                balance = balance + amount;
+            }
+            public string GetName()
+            {
+                return this.name;
+            }  
+            public void SetName(string accountName)
+            {
+                this.name = accountName;
+            }
+        }
+        public class CustomerAccount : Account
+        {
+            public CustomerAccount(string accountName, decimal amount)
+            {
+                this.SetName(accountName);
+                this.SetBalance(amount);
+            }
+            public override string RudeLetterString()
+            {
+                return "You are overdrawn";
+            }
+        }
+        public class BabyAccount : Account
+        {
+            public override bool WithdrawFunds(decimal amount)
+            {
+                if (amount > 10)
+                {
+                    return false;
+                }
+                return base.WithdrawFunds(amount);
+            }
+            public override string RudeLetterString()
+            {
+                return "Tell daddy you are overdrawn";
+            }
+        } 
+
+        interface IBank 
+        { 
+            IAccount FindAccount (string name);
+            bool StoreAccount (IAccount account);
+            void PrintAccountList();
+        }
+
+        public class ArrayBank : IBank
+        {
+            private IAccount[] accounts;
+
+            public ArrayBank(int bankSize)
+            {
+                accounts = new IAccount[bankSize];
+            }
+
+            public bool StoreAccount(IAccount account)
+            {
+                int position = 0;
+                for (position = 0; position < accounts.Length; position++)
+                {
+                    if (accounts[position] == null)
+                    {
+                        accounts[position] = account;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public IAccount FindAccount(string name)
+            {
+                int position = 0;
+                for (position = 0; position < accounts.Length; position++)
+                {
+                    if (accounts[position] == null)
+                    {
+                        continue;
+                    }
+                    if (accounts[position].GetName() == name)
+                    {
+                        return accounts[position];
+                    }
+                }
+                return null;
+            }
+            public void PrintAccountList()
+            {
+                int position = 0;
+                for (position = 0; position < accounts.Length; position++)
+                {
+                    if (accounts[position] == null)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine("AccountName : " + accounts[position].GetName() + "  :  " + accounts[position].GetBalance());
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            IBank friendlyBank = new ArrayBank(50); 
+            IAccount account = new CustomerAccount("Rob", 0); 
+            if (friendlyBank.StoreAccount(account)) 
+            {
+                Console.WriteLine ( "Account stored OK" );
+            }
+            account = new CustomerAccount("Rudi", 1000);
+            if (friendlyBank.StoreAccount(account))
+            {
+                Console.WriteLine("Account stored OK");
+            }
+            account = new CustomerAccount("Toni", 1100);
+            if (friendlyBank.StoreAccount(account))
+            {
+                Console.WriteLine("Account stored OK");
+            }
+            friendlyBank.PrintAccountList();
+            Console.ReadKey();
+
+        }
+    }
+}
